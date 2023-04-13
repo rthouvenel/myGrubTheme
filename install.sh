5c3cd0efb4a3f065a46dbcd04f2f2cb442f852f1
mon nom <ryan.thouvenel@epitech.eu>
2023-04-13 14:38:09 +0200

    nya

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ select l in "${INSTALLER_LANG_NAMES[@]}"; do
 done < /dev/tty
 
 echo 'Fetching and unpacking theme'
-wget -O - https://github.com/rthouvenel/${GRUB_THEME}/archive/nya.tar.gz | tar -xzf - --strip-components=1
+wget -O - https://github.com/rthouvenel/${GRUB_THEME}/archive/master.tar.gz | tar -xzf - --strip-components=1
 
 if [[ "$INSTALLER_LANG" != "English" ]]; then
     echo "Changing language to ${INSTALLER_LANG}"
